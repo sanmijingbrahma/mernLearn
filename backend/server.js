702ef8de937659a7679eb497d5edae01a7abf72b
sanmijingbrahma <bsanmijing@gmail.com>
2024-10-14 23:55:01 +0530

    Connected to DB and Created Create Route and list Route

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
-const express = require("express")
+const { configDotenv } = require("dotenv");
+const express = require("express");
+const dotenv = require("dotenv")
+const { default: mongoose } = require("mongoose");
+
 
 const app = express();
-
+dotenv.config();
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json()) // a middleware to parse the json file
 
@@ -17,6 +22,23 @@ app.use((req,res,next)=>{
 })
 
 
+// To connect to DB{mernlearning} from the backend.
+const MONGO_URL = 'mongodb://localhost:27017/mernlearning'; //DB url
+mongoose.connect(MONGO_URL, {useNewUrlParser:true, useUnifiedTopology:true})
+.then(()=>{console.log('Connected to DB.');
+}).catch(()=>{
+    console.error('DB connection failed!');
+})
+
+
+const taskSchema = new mongoose.Schema({
+    title: String,
+    completed : Boolean
+})
+
+const Task = mongoose.model("Task",taskSchema)
+
+
 // Routes
 
 app.get("/api/greet", (req,res)=>{
@@ -29,11 +51,31 @@ app.post("/api/echo", (req,res)=>{
     res.json({echo:req.body})
 })
 
+// to create task
+app.post("/api/tasks", async (req,res)=>{
+    try {
+        const newTask = new Task(req.body)
+        await newTask.save();
+        res.status(201).json(newTask)
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
+})
+
+// to list tasks
+app.get("/api/tasks", async(req,res)=>{
+    try {
+        const tasks = await Task.find()
+        res.status(201).json(tasks);
+    } catch (k) {
+       res.status(400).json({error:error.message}) 
+    }
+})
 app.use((req,res)=>{
     res.status(404).json({error: "Not Found"})
 })
 
 
-app.listen(8000,()=>{
-    console.log("Listining on 8000")
+app.listen(PORT,()=>{
+    console.log(`Server is running on http://localhost:${PORT}/`)
 })
